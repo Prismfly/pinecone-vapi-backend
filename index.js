cc11a4cf8e1bca5882ec1f6a26139bf7fffcc966
Alex <afbelardi@gmail.com>
2025-05-16 15:13:20 -0400

    able to send a query successfully to the server and get a response back

--- index.js
+++ index.js
@@ -7,14 +7,20 @@ import dotenv from "dotenv";
 import cors from "cors";
 
 dotenv.config();
-app.use(express.json());
 app.use(cors());
 
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 app.use(bodyParser.json(), urlencodedParser);
 
+/* Middleware */
+app.use(express.json());
+
 app.post("/vapi-query", async (req, res) => {
   const userQuery = req.body.query;
+
+  if (!userQuery) {
+    return res.status(400).json({ error: "Missing query in request body" });
+  }
 
   try {
     const results = await queryPinecone(userQuery);
